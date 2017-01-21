7e6f5bb2ba7fd4739c168f70c4999ba9398a82a8
Gil Falkovitch <gilfalko@gmail.com>
2017-01-21 21:48:04 +0200

    Write permissions OK

--- themoviekeeper/src/main/java/com/example/themoviekeeper/ui/MainActivity.java
+++ themoviekeeper/src/main/java/com/example/themoviekeeper/ui/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.themoviekeeper.ui;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -66,8 +70,75 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPreferencesHelpFirst();
 
-
         floatingActionButtonListener();
+
+        checkAppPermissions();
+
+    }
+
+    private void checkAppPermissions() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+            requestAppPermissions();
+
+        }
+
+    }
+
+    private void requestAppPermissions() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+
+            case 1:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+
+                    AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+                    alertDialog.setTitle("Permissions");
+                    alertDialog.setMessage("This app MUST have storage write permissions. Please accept it.");
+                    alertDialog.setIcon(R.drawable.ic_check_circle_black_48dp);
+                    alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                    1);
+
+                        }
+                    });
+
+                    alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "NO", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            finish();
+
+                        }
+                    });
+
+
+                    alertDialog.setCancelable(false);
+                    alertDialog.show();
+
+
+                }
+        }
 
     }
 
